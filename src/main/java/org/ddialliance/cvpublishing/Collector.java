88d438196bbf4a8646e2b8bd3f6e6abb12c74629
Oliver Hopt <oliver.hopt@gesis.org>
2026-06-30 15:10:01 +0200

    Cleanup

--- src/main/java/org/ddialliance/cvpublishing/Collector.java
+++ src/main/java/org/ddialliance/cvpublishing/Collector.java
@@ -1,13 +1,6 @@
 package org.ddialliance.cvpublishing;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
@@ -16,6 +9,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -37,6 +31,9 @@ public class Collector
 		try
 		{
 			TransformerFactory tf = TransformerFactory.newInstance( "net.sf.saxon.TransformerFactoryImpl", null );
+			InputStream xslStreamRDF = this.getClass().getClassLoader()
+					.getResourceAsStream( "transformation/SKOSCleanup.xsl" );
+			Transformer tRDF = tf.newTransformer( new StreamSource( xslStreamRDF ) );
 			InputStream xslStreamDDI = this.getClass().getClassLoader()
 					.getResourceAsStream( "transformation/SKOS2CodeList.xsl" );
 			Transformer tDDI = tf.newTransformer( new StreamSource( xslStreamDDI ) );
@@ -86,29 +83,130 @@ public class Collector
 			{
 				JSONObject vocabulary = (JSONObject) object;
 				String vocabularyName = vocabulary.get( "notation" ).toString();
+				System.out.println( vocabularyName );
 				if ( true )
 				{
 					Set<String> allSlVersionsForVoc = ((JSONObject) ((JSONObject) allPublishedVocabularies.get( vocabularyName ))
 							.get( "en(SL)" )).keySet();
-
+					Set<String> allLangsForVoc = ((JSONObject) allPublishedVocabularies.get( vocabularyName )).keySet();
+					HashMap<String, HashMap<String, VersionStructure>> allVersionsToProcess = new HashMap<String, HashMap<String, VersionStructure>>();
 					for ( Iterator<String> iterator = allSlVersionsForVoc.iterator(); iterator.hasNext(); )
 					{
 						String vocabularyVersion = iterator.next();
-
-						downloadRDF( rootDir, vocabularyVersion, vocabularyName, ".rdf", "application/xml" );
-						Thread.sleep( 3000 );
-						if ( !vocabularyName.equals( "ModeOfCollection" ) || !vocabularyVersion.startsWith( "4.0." ) )
-							download( rootDir, vocabularyVersion, vocabularyName, ".pdf", "application/pdf" );
-						// Thread.sleep( 3000 );
-						// downloadHTML( rootDir, vocabularyVersion, vocabularyName, ".html",
-						// "application/xhtml+xml" );
-
-						// create HTML from RDF
-						transformRDF( rootDir, vocabularyVersion, vocabularyName, tDDI, ".rdf", ".xml" );
-						// create DDI-XML
-						transformRDF( rootDir, vocabularyVersion, vocabularyName, tHTML, ".rdf", ".html" );
-						Thread.sleep( 9000 );
+						String[] versionParts = vocabularyVersion.split( "\\." );
+						Integer subminor = Integer.valueOf( versionParts[2] );
+						if ( subminor == 0 )
+						{
+							subminor = Integer.valueOf( 1 );
+						}
+						if ( !allVersionsToProcess.containsKey( vocabularyVersion ) )
+							allVersionsToProcess.put(
+									versionParts[0] + "." + versionParts[1] + "." + subminor,
+									new HashMap<String, VersionStructure>() );
 					}
+					for ( Iterator<String> iterator = allLangsForVoc.iterator(); iterator.hasNext(); )
+					{
+						String lang = iterator.next();
+						Set<String> langVersions = ((JSONObject) ((JSONObject) allPublishedVocabularies.get( vocabularyName ))
+								.get( lang )).keySet();
+						for ( Iterator<String> versionIterator = langVersions.iterator(); versionIterator.hasNext(); )
+						{
+							String vocabularyVersion = versionIterator.next();
+							String[] versionParts = vocabularyVersion.split( "\\." );
+							VersionStructure vs = new VersionStructure( versionParts[0], versionParts[1], versionParts[2], lang );
+							Integer subminor = Integer.valueOf( versionParts[2] );
+							if ( lang.startsWith( "en" ) && subminor.intValue() == 0 )
+							{
+								subminor = Integer.valueOf( 1 );
+							}
+							Integer slSubminor = Integer.valueOf( 20 );
+							for ( Iterator<String> slVersionIterator = allVersionsToProcess.keySet().iterator(); slVersionIterator
+									.hasNext(); )
+							{
+								String aSlVersion = slVersionIterator.next();
+								if ( aSlVersion.startsWith( vs.getMajorMinor() ) )
+								{
+									Integer aSubminor = Integer.valueOf( aSlVersion.substring( aSlVersion.indexOf( ".", 2 ) + 1 ) );
+									if ( subminor <= aSubminor && subminor < slSubminor )
+									{
+										slSubminor = aSubminor;
+									}
+								}
+							}
+							HashMap<String, VersionStructure> version = allVersionsToProcess.get( vs.getMajorMinor() + "." + slSubminor );
+							if ( slSubminor < 20 )
+							{
+								if ( version.containsKey( lang ) )
+								{
+									version.replace( lang, vs );
+								}
+								else
+								{
+									version.put( lang, vs );
+								}
+							}
+						}
+					}
+					for ( Iterator<String> iterator = allVersionsToProcess.keySet().iterator(); iterator.hasNext(); )
+					{
+						String aVersionNr = iterator.next();
+						String allLangsQuery = "";
+						HashMap<String, VersionStructure> allLangVersions = allVersionsToProcess.get( aVersionNr );
+						String versionNr = allLangVersions.get( "en(SL)" ).getVersion();
+						for ( Iterator<String> langIterator = allLangVersions.keySet().iterator(); langIterator.hasNext(); )
+						{
+							String lang = langIterator.next();
+							VersionStructure aLang = allLangVersions.get( lang );
+							allLangsQuery += lang.substring( 0, lang.indexOf( "(" ) );
+							allLangsQuery += "-";
+							allLangsQuery += aLang.getMajor();
+							allLangsQuery += ".";
+							allLangsQuery += aLang.getMinor();
+							allLangsQuery += ".";
+							allLangsQuery += aLang.getSubminor();
+							if ( langIterator.hasNext() )
+							{
+								allLangsQuery += "_";
+							}
+						}
+						boolean available = downloadRDF( rootDir, versionNr, vocabularyName, allLangsQuery, ".rdf",
+								"application/rdf+xml", tRDF );
+						Thread.sleep( 3000 );
+						if ( available )
+						{
+							// download PDF from CVS
+							//download( rootDir, versionNr, vocabularyName, allLangsQuery, ".pdf",
+							//		"application/pdf" );
+							// create HTML from RDF
+							System.out.println( "transforming to DDI" );
+							transformRDF( rootDir, versionNr, vocabularyName, tDDI, ".rdf", ".xml" );
+							// create DDI-XML
+							System.out.println( "transforming to HTML" );
+							transformRDF( rootDir, versionNr, vocabularyName, tHTML, ".rdf", ".html" );
+							Thread.sleep( 9000 );
+						}
+						else
+						{
+							System.out.println( "unavailable" );
+						}
+						System.out.println( versionNr + ": " + allLangsQuery );
+					}
+
+					/*
+					 * for ( Iterator<String> iterator = allSlVersionsForVoc.iterator();
+					 * iterator.hasNext(); ) { String vocabularyVersion = iterator.next();
+					 * 
+					 * downloadRDF( rootDir, vocabularyVersion, vocabularyName, ".rdf",
+					 * "application/rdf+xml" ); Thread.sleep( 3000 ); download( rootDir,
+					 * vocabularyVersion, vocabularyName, ".pdf", "application/pdf" ); //
+					 * Thread.sleep( 3000 ); // downloadHTML( rootDir, vocabularyVersion,
+					 * vocabularyName, ".html", // "application/xhtml+xml" );
+					 * 
+					 * // create HTML from RDF transformRDF( rootDir, vocabularyVersion,
+					 * vocabularyName, tDDI, ".rdf", ".xml" ); // create DDI-XML transformRDF(
+					 * rootDir, vocabularyVersion, vocabularyName, tHTML, ".rdf", ".html" );
+					 * Thread.sleep( 9000 ); }
+					 */
 				}
 			}
 		}
@@ -122,13 +220,14 @@ public class Collector
 			File rootDir,
 			String vocabularyVersion,
 			String vocabularyName,
+			String langQuery,
 			String outputFileSuffix,
 			String contentType ) throws IOException
 	{
 		try
 		{
 			String urlString = "https://vocabularies.cessda.eu/v2/vocabularies/"
-					+ vocabularyName + "/" + vocabularyVersion;
+					+ vocabularyName + "/" + vocabularyVersion + "?languageVersion=" + langQuery;
 			File vocDir = new File( rootDir, vocabularyName + "/" + vocabularyVersion );
 			vocDir.mkdirs();
 			File toDeleteFile = new File( vocDir, vocabularyName + outputFileSuffix );
@@ -152,6 +251,7 @@ public class Collector
 			File rootDir,
 			String vocabularyVersion,
 			String vocabularyName,
+			String langQuery,
 			String outputFileSuffix,
 			String contentType ) throws IOException
 	{
@@ -181,7 +281,7 @@ public class Collector
 		try
 		{
 			String urlString = "https://vocabularies.cessda.eu/v2/vocabularies/"
-					+ vocabularyName + "/" + vocabularyVersion;
+					+ vocabularyName + "/" + vocabularyVersion + "?languageVersion=" + langQuery;
 			File vocDir = new File( rootDir, vocabularyName + "/" + vocabularyVersion );
 			vocDir.mkdirs();
 			File toDeleteFile = new File( vocDir, vocabularyName + outputFileSuffix );
@@ -220,12 +320,14 @@ public class Collector
 		}
 	}
 
-	private void downloadRDF(
+	private boolean downloadRDF(
 			File rootDir,
 			String vocabularyVersion,
 			String vocabularyName,
+			String langQuery,
 			String outputFileSuffix,
-			String contentType ) throws IOException
+			String contentType,
+			Transformer t ) throws IOException
 	{
 		String vocabularyVersionShort = vocabularyVersion.substring( 0, vocabularyVersion.indexOf( ".", 2 ) );
 
@@ -237,7 +339,7 @@ public class Collector
 		try
 		{
 			String urlString = "https://vocabularies.cessda.eu/v2/vocabularies/"
-					+ vocabularyName + "/" + vocabularyVersion;
+					+ vocabularyName + "/" + vocabularyVersion + "?languageVersion=" + langQuery;
 			File vocDir = new File( rootDir, vocabularyName + "/" + vocabularyVersion );
 			vocDir.mkdirs();
 			File toDeleteFile = new File( vocDir, vocabularyName + outputFileSuffix );
@@ -249,18 +351,31 @@ public class Collector
 					.header( "accept", contentType )
 					.build();
 			HttpResponse<String> response = client.send( request, BodyHandlers.ofString() );
+			System.out.println( response );
+			if ( response.statusCode() != 200 )
+				return false;
 			String body = response.body();
-
+			/*
 			body = body.replaceAll( toReplace, replacement );
+
 			FileWriter writer = new FileWriter( file );
 			writer.write( body );
 			writer.close();
-			System.out.println( response );
+			*/
+			FileOutputStream fos = new FileOutputStream( file );
+			StringReader reader = new StringReader(body);
+			t.setParameter("name", vocabularyName);
+			t.setParameter("version", vocabularyVersion);
+			t.transform( new StreamSource( reader ), new StreamResult( fos ) );
+			fos.close();
+
 		}
 		catch (Exception ex)
 		{
-
+			ex.printStackTrace();
+			return false;
 		}
+		return true;
 	}
 
 	private void transformRDF(
@@ -283,4 +398,49 @@ public class Collector
 		fos.close();
 	}
 
+	private class VersionStructure
+	{
+		private Integer major = Integer.valueOf( 0 );
+		private Integer minor = Integer.valueOf( 0 );
+		private Integer subminor = Integer.valueOf( 0 );
+		private String lang = "";
+
+		public VersionStructure( String major, String minor, String subminor, String lang )
+		{
+			this.major = Integer.valueOf( major );
+			this.minor = Integer.valueOf( minor );
+			this.subminor = Integer.valueOf( subminor );
+			this.lang = lang;
+		}
+
+		public Integer getMajor()
+		{
+			return major;
+		}
+
+		public Integer getMinor()
+		{
+			return minor;
+		}
+
+		public Integer getSubminor()
+		{
+			return subminor;
+		}
+
+		public String getLang()
+		{
+			return lang;
+		}
+
+		public String getMajorMinor()
+		{
+			return major + "." + minor;
+		}
+
+		public String getVersion()
+		{
+			return major + "." + minor + "." + subminor;
+		}
+	}
 }
--- src/main/resources/transformation/SKOSCleanup.xsl
+++ src/main/resources/transformation/SKOSCleanup.xsl
@@ -2,7 +2,6 @@
 <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
     xmlns:xs="http://www.w3.org/2001/XMLSchema"
     xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-    xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
     xmlns:skos="http://www.w3.org/2004/02/skos/core#"
     xmlns:owl="http://www.w3.org/2002/07/owl#"
     xmlns:dcterms="http://purl.org/dc/terms/"
@@ -10,7 +9,18 @@
     version="2.0">
     
     <xsl:output method="xml" indent="yes"></xsl:output>
-    
+
+	<xsl:param name="name"/>
+	<xsl:param name="version"/>
+
+	<xsl:variable name="base">
+		<xsl:text>http://rdf-vocabulary.ddialliance.org/cv/</xsl:text>
+		<xsl:value-of select="$name"/>
+		<xsl:text>/</xsl:text>
+		<xsl:value-of select="$version"/>
+		<xsl:text>/</xsl:text>
+	</xsl:variable>
+
     <xsl:template match="rdf:RDF">
     	<rdf:RDF>
     		<xsl:apply-templates select="rdf:Description[rdf:type/@rdf:resource='http://www.w3.org/2004/02/skos/core#ConceptScheme']" mode="CV"/>
@@ -20,26 +30,19 @@
     
     <xsl:template match="rdf:Description" mode="CV">
     	<rdf:Description>
-    		<xsl:copy-of select="@rdf:about"/>
+			<xsl:attribute name="rdf:about"><xsl:value-of select="$base"/></xsl:attribute>
     		<xsl:copy-of select="dcterms:identifier"/>
     		<xsl:copy-of select="rdf:type"/>
-    		<xsl:copy-of select="dcterms:identifier"/>
-    		<xsl:copy-of select="dcterms:isVersionOf"/>
+			<dcterms:isVersionOf>
+				<xsl:attribute name="rdf:resource">
+					<xsl:text>http://rdf-vocabulary.ddialliance.org/cv/</xsl:text>
+					<xsl:value-of select="$name"/>
+					<xsl:text>/</xsl:text>
+				</xsl:attribute>
+			</dcterms:isVersionOf>
     		<xsl:copy-of select="owl:priorVersion"/>
     		<xsl:copy-of select="skos:notation"/>
-    		<xsl:copy-of select="skos:prefLabel[not(text()='')]|skos:definition[not(text()='')]"/>
-    		<xsl:for-each select="dcterms:title">
-    			<skos:prefLabel>
-    				<xsl:copy-of select="@xml:lang"/>
-    				<xsl:value-of select="text()"/>
-    			</skos:prefLabel>
-    		</xsl:for-each>
-    		<xsl:for-each select="dcterms:description">
-    			<skos:definition>
-    				<xsl:copy-of select="@xml:lang"/>
-    				<xsl:value-of select="text()"/>
-    			</skos:definition>
-    		</xsl:for-each>
+    		<xsl:copy-of select="skos:prefLabel[not(text()='')]|skos:definition[not(text()='')]|dcterms:title[not(text()='')]|dcterms:description[not(text()='')]"/>
     		<xsl:copy-of select="owl:versionInfo"/>
     		<xsl:copy-of select="dcterms:license"/>
     		<xsl:copy-of select="dcterms:rights"/>
@@ -47,31 +50,44 @@
     		<xsl:copy-of select="dcterms:issued"/>
     		<xsl:copy-of select="skos:historyNote"/>
     	    <xsl:copy-of select="skos:changeNote"/>
-    	    <xsl:copy-of select="skos:hasTopConcept"/>
+			<xsl:for-each select="skos:hasTopConcept">
+				<xsl:variable name="idplus" select="substring-after(@rdf:resource, concat($name, '/'))"/>
+				<xsl:variable name="id" select="substring-after($idplus, '/')"/>
+				<skos:hasTopConcept>
+					<xsl:attribute name="rdf:resource"><xsl:value-of select="$base"/><xsl:value-of select="$id"/></xsl:attribute>
+				</skos:hasTopConcept>
+			</xsl:for-each>
     	</rdf:Description>
     </xsl:template>
     
     <xsl:template match="rdf:Description" mode="Entry">
-		<xsl:variable name="code" select="skos:notation"/>
-    	<rdf:Description>
-    	    <xsl:copy-of select="@rdf:about"/>
+		<xsl:variable name="idplus" select="substring-after(@rdf:about, concat($name, '/'))"/>
+		<xsl:variable name="id" select="substring-after($idplus, '/')"/>
+		<rdf:Description>
+			<xsl:attribute name="rdf:about"><xsl:value-of select="$base"/><xsl:value-of select="$id"/></xsl:attribute>
     	    <xsl:copy-of select="rdf:type"/>
-    	    <xsl:copy-of select="dcterms:isVersionOf"/>
-    		<xsl:copy-of select="owl:priorVersion"/>
-    		<xsl:copy-of select="owl:priorVersion"/>
-    		<xsl:copy-of select="skos:inScheme"/>
-    		<xsl:copy-of select="skos:topConceptOf"/>
+			<xsl:for-each select="skos:inScheme">
+				<skos:inScheme>
+					<xsl:attribute name="rdf:resource"><xsl:value-of select="$base"/></xsl:attribute>
+				</skos:inScheme>
+			</xsl:for-each>
     		<xsl:copy-of select="dcterms:created"/>
     		<xsl:copy-of select="skos:changeNote"/>
     		<xsl:copy-of select="skos:notation"/>
-    		<xsl:copy-of select="skos:prefLabel[not(text()='')]|skos:definition[not(text()='')]"/>
-    	    <xsl:copy-of select="skos:narrower"/>
-   		 <xsl:variable name="myConceptId" select="@rdf:about"/>
+			<xsl:copy-of select="skos:prefLabel[not(text()='')]|skos:definition[not(text()='')]|dcterms:title[not(text()='')]|dcterms:description[not(text()='')]"/>
+			<xsl:for-each select="skos:narrower">
+				<xsl:variable name="ridplus" select="substring-after(@rdf:resource, concat($name, '/'))"/>
+				<xsl:variable name="rid" select="substring-after($ridplus, '/')"/>
+				<skos:narrower>
+					<xsl:attribute name="rdf:resource"><xsl:value-of select="$base"/><xsl:value-of select="$rid"/></xsl:attribute>
+				</skos:narrower>
+			</xsl:for-each>
+   		    <xsl:variable name="myConceptId" select="@rdf:about"/>
     		<xsl:for-each select="//rdf:Description[skos:narrower/@rdf:resource=$myConceptId]">
+				<xsl:variable name="ridplus" select="substring-after(@rdf:about, concat($name, '/'))"/>
+				<xsl:variable name="rid" select="substring-after($ridplus, '/')"/>
 	    		<skos:broader>
-		    		<xsl:attribute name="rdf:resource">
-		    			<xsl:value-of select="@rdf:about"/>
-		    		</xsl:attribute>
+					<xsl:attribute name="rdf:resource"><xsl:value-of select="$base"/><xsl:value-of select="$rid"/></xsl:attribute>
 	    		</skos:broader>
     		</xsl:for-each>
     	</rdf:Description>
